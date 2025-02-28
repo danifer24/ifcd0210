615eb78ec3cd96902768d42316c1f76bb1fcfd69
danifer24 <danielvf24@gmail.com>
2025-02-28 15:40:13 +0100

    Ejercicio3_arrays

---
+++ 4_arrays/3_agenda.php
@@ -0,0 +1,14 @@
+<?php
+
+$agenda = [
+    "Manolo"=> "695012395",
+    "Pili"=> "650234023",
+    "Juanin"=> "612305123",
+    "Paco" => "985123567",
+];
+
+
+foreach ($agenda as $i => $value) {
+    echo "El numero de $i es: $value";
+    echo "<br>";
+}
