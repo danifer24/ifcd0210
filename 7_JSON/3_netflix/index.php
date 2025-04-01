4fdd45e9c46bd2173f672f58afc27a9415778817
danifer24 <danielvf24@gmail.com>
2025-04-01 18:34:19 +0200

    7_3 filtro por genero

--- 7_JSON/3_netflix/index.php
+++ 7_JSON/3_netflix/index.php
@@ -9,6 +9,41 @@ $netflix_array = json_decode($netflix, true);
 
 // iterar y mostrar dicho array
 
+function mostrarPelis($array)
+{
+    echo "<table border>";
+    foreach ($array as $peli) {
+        echo "
+            <tr>
+                <td> $peli[titulo] </td>
+                <td> $peli[año]</td>
+                <td> $peli[director]</td>";
+        echo "<td>";
+        foreach ($peli["genero"] as $genero) {
+            echo "$genero ";
+        }
+        echo "</td>";
+    }
+    echo "</table>";
+}
+
+//Opcionales:
+
+// 1) añadir CSS (flexbos, formato tarjetitas, donde los géneros pueden ser badges)
+// 2) formulario de filtro por género  (pista: utilizar función in_array())
+function filtrarPelis($genero)
+{
+    global $netflix_array;
+    $array_filtrado = array_filter(
+        $netflix_array,
+        function ($elemento) use ($genero) {
+            return in_array($genero, $elemento["genero"]);
+        }
+    );
+    return $array_filtrado;
+}
+// 3) Repetir el ejercicio pero sólo con JavaScript
+// 4) Filtrado dinámico de las películas
 ?>
 
 <!DOCTYPE html>
@@ -20,34 +55,33 @@ $netflix_array = json_decode($netflix, true);
     <title>Document</title>
 </head>
 
-
 <body>
-    <table border>
-        <tr>
-            <th>Título</th>
-            <th>Año</th>
-            <th>Director</th>
-            <th>Género</th>
-        </tr>
-        <?php
-        foreach ($netflix_array as $peli) {
-            echo "
-                <tr>
-                    <td> $peli[titulo] </td>
-                    <td> $peli[año]</td>
-                    <td> $peli[director]</td>";
-            echo "<td>";
-            foreach ($peli["genero"] as $genero) {
-                echo "$genero ";
-            }
-            echo "</td>";
-        }
-        ?>
 
-    </table>
+    <tr>
+        <th>Título</th>
+        <th>Año</th>
+        <th>Director</th>
+        <th>Género</th>
+    </tr>
+    <?php
+    mostrarPelis($netflix_array);
+    ?>
+
+
+    <form>
+        <label for="genero">Filtrado por género</label>
+        <input id="genero" name="genero" type="text">
+        <input id="filtrar" type="submit" title="Filtrar">
+
+    </form>
+
+    <?php
+    if (isset($_GET["genero"])) {
+        $filtradas = filtrarPelis($_GET["genero"]);
+        mostrarPelis($filtradas);
+    };
+
+    ?>
 </body>
 
 </html>
-
-
-
